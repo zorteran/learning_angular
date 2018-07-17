78fad1dd9e17fc6b6b9387280821912972ab5041
Maciej <Maciej@DESKTOP-MUQ3U08>
2018-07-17 13:04:30 +0200

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
